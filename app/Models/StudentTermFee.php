282475f7f9bf4e55de27a7bc3e1841d7a711a481
kennedy mokua <mogakakennedy254@gmail.com>
2025-02-07 08:48:23 +0300

    updated and added added student term fee model

---
+++ app/Models/StudentTermFee.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class StudentTermFee extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'student_id',
+        'term_id',
+        'amount',
+        'payment_date',
+        'payment_mode'
+    ];
+
+
+    public function student(): BelongsTo
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    public function term(): BelongsTo
+    {
+        return $this->belongsTo(Term::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($fee) {
+            if ($fee->student) {
+                $fee->student->updateTermPayments();
+            }
+        });
+    }
+}
