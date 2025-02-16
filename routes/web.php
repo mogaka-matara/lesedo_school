d69a6ba0128906b82b5152dfc01443a12defc3f8
kennedy mokua <mogakakennedy254@gmail.com>
2025-02-16 14:02:03 +0300

    added  more routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Backend\AcademicYearController;
+use App\Http\Controllers\Backend\BookController;
+use App\Http\Controllers\Backend\FeeComponentController;
 use App\Http\Controllers\Backend\FeeController;
 use App\Http\Controllers\Backend\GradeController;
+use App\Http\Controllers\Backend\LibraryController;
+use App\Http\Controllers\Backend\PromotionController;
 use App\Http\Controllers\Backend\StudentController;
+use App\Http\Controllers\Backend\SubjectController;
 use App\Http\Controllers\Backend\TermController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +39,26 @@ Route::middleware('auth')->group(function () {
 
     //terms routes
     Route::resource('term', TermController::class);
+
+    Route::resource('fee-component', FeeComponentController::class);
+
+    Route::resource('academic-year', AcademicYearController::class);
+
+
+    Route::get('/students-by-grade', [PromotionController::class, 'getStudentsByGrade'])->name('students.by.grade');
+    Route::resource('promotion', PromotionController::class);
+
+    //library Routes
+    Route::resource('library', BookController::class);
+
+
+    Route::get('library-borrowed-books', [LibraryController::class, 'index'])->name('borrowedBooks');
+    Route::post('library/borrow', [LibraryController::class, 'borrow'])->name('library.borrow-store');
+    Route::put('return/{id}', [LibraryController::class, 'returnBook'])->name('library.return');
+
+    //subject creation routes
+    Route::get('all-subjects', [SubjectController::class, 'index'])->name('all.subjects');
+    Route::post('all-subjects', [SubjectController::class, 'getSubjects'])->name('store.subject');
 });
 
 require __DIR__.'/auth.php';
