aee084b74c261d55f117188ebdec6629ef6b2962
kennedy mokua <mogakakennedy254@gmail.com>
2025-02-21 21:57:19 +0300

    updated Supply Desks and chairs history

--- app/Models/SupplyHistory.php
+++ app/Models/SupplyHistory.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SupplyHistory extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['item_id', 'quantity', 'supply_date'];
+
+
 }
