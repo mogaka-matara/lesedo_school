77ef33ece961958b96a3ddea3419e521557cdb37
kennedy mokua <mogakakennedy254@gmail.com>
2025-02-22 13:11:41 +0300

    added the tables

---
+++ database/migrations/2025_02_19_135342_create_uniform_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('uniform_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('student_id');
+            $table->json('components');
+            $table->double('total_price');
+            $table->date('issue_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('uniform_transactions');
+    }
+};
